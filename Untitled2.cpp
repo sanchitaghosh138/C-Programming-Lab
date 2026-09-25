ba10659c85eedffc4cac6df12b038a13d40b6d27
sanchitaghosh138 <sanchitaghosh138@gmail.com>
2026-09-25 17:19:35 +0530

    Add files via upload

--- Untitled2.cpp
+++ Untitled2.cpp
@@ -1,11 +1,25 @@
-#include <stdio.h>
+#include<stdio.h>
 int main()
 {
-	int i;
-	while(i>=10,i%2==0)
+	int i=1, c=1, a=1, n;
+	long int fact=1,sum=0;
+	printf("Enter the no of terms:");
+	scanf("%d",&n);
+	
+	while(c<=n)
 	{
-		printf("%d\n",i);
-		i++;
+		i=1;
+		fact=1;
+		while(i<=a)
+		{
+			fact=fact*i;
+			i++;
+		}
+		sum=sum+fact;
+		c++;
+		a=a+2;	
 	}
+	printf("sum of the numbers=%d",sum);
 	return 0;
+	
 }
